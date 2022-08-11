224340353b736bf9fa07630a74075d6a0a49f291
Preston Moore <pkm266@nyu.edu>
2022-08-11 17:00:53 -0400

    Initial commit

---
+++ deploy-commands.js
@@ -0,0 +1,17 @@
+// Add commands to the commands array and run this file to deploy them to the
+// magical place inside discord where a bots commands live
+const { SlashCommandBuilder, Routes } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [
+    new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+    new SlashCommandBuilder().setName('pong').setDescription('Replies with ping!')
+]
+    .map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token, targetChannel } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Login to Discord with your client's token
+client.login(token);
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const { commandName, channelId } = interaction;
+
+    if (channelId === targetChannel) {
+        if (commandName === 'ping') {
+            await interaction.reply('Pong!');
+        } else if (commandName === 'pong') {
+            await interaction.reply('Segmentation Fault (Core Dumped)');
+        }
+    }
+});
+
